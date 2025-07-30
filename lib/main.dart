31853905805555bb99f6b6ad2942a3977f5b1bb2
sharada417 <sharada.kanchana2001@gmail.com>
2025-07-30 15:51:53 +0530

    Initail commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(), // default main page
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to My App'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the main pa',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
